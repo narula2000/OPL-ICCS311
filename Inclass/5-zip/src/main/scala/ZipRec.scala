71a9d1517ade152504f4d6871ba5c086918494f5
Vikrom Narula <djnarula@gmail.com>
2020-09-25 13:58:49 +0700

    Added Concat for zip

--- Inclass/5-zip/src/main/scala/ZipRec.scala
+++ Inclass/5-zip/src/main/scala/ZipRec.scala
@@ -25,10 +25,26 @@ object ZipRec extends App {
     inner(zipped, a, b)
   }
 
+  def unzipConcat(zipped: List[(Int, Int)]): (List[Int], List[Int]) = {
+    var a = List()
+    var b = List()
+    @tailrec
+    def inner(
+        xs: List[(Int, Int)],
+        a: List[Int],
+        b: List[Int]): (List[Int], List[Int]) =
+      xs match {
+        case head :: next => inner(next, a ::: List(head._1), b ::: List(head._2))
+        case Nil => (a, b)
+      }
+    inner(zipped, a, b)
+  }
+
   val testUnzip = List((3, 6), (2, 1), (5, 9))
   val testZip1 = List(3, 2, 5)
   val testZip2 = List(6, 1, 9)
 
   println(zip(testZip1, testZip2))
   println(unzip(testUnzip))
+  println(unzipConcat(testUnzip))
 }
