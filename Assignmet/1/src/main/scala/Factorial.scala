2252b5ebbdaf803b77a79354a65fa16f73808b94
Vikrom Narula <djnarula@gmail.com>
2020-09-26 15:41:02 +0700

    Added Asssingment 1 files and project     Finished Question 1 Factorial

---
+++ Assignmet/1/src/main/scala/Factorial.scala
@@ -0,0 +1,17 @@
+import scala.math._
+import scala.annotation.tailrec
+
+object Factorial extends App {
+  def factorial(n: Int): Long = {
+    @tailrec
+    def inner(acc: Long, n: Int): Long = {
+      if (n <= 1) acc
+      else inner(n * acc, n - 1)
+    }
+    inner(1, n)
+  }
+
+  println(factorial(0) + " == 1")
+  println(factorial(10) + " == 3628800")
+  println(factorial(19) + " == 12164510040883200")
+}
