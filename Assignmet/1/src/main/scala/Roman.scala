809f496b231da7598f41d900a26ae2c5ac98387f
Vikrom Narula <djnarula@gmail.com>
2020-09-29 11:07:55 +0700

    Need to fix function

--- Assignmet/1/src/main/scala/Roman.scala
+++ Assignmet/1/src/main/scala/Roman.scala
@@ -20,62 +20,64 @@ object Roman extends App {
 
   val answer = new StringBuilder("")
 
+  def parser(value: String): String = value.replace("Some(", "").replace(")", "")
+
   def toRoman(n: Int): String = {
     if (n >= 1000) {
-      answer.append(romeNum.get(1000).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(1000).toString()))
       toRoman(n - 1000)
     }
     else if (n >= 900) {
-      answer.append(romeNum.get(900).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(900).toString()))
       toRoman(n - 900)
     }
     else if (n >= 500) {
-      answer.append(romeNum.get(500).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(500).toString()))
       toRoman(n - 500)
     }
     else if (n >= 400) {
-      answer.append(romeNum.get(400).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(400).toString()))
       toRoman(n - 400)
     }
     else if (n >= 100) {
-      answer.append(romeNum.get(100).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(100).toString()))
       toRoman(n - 100)
     }
     else if (n >= 90) {
-      answer.append(romeNum.get(90).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(90).toString()))
       toRoman(n - 90)
     }
     else if (n >= 50) {
-      answer.append(romeNum.get(50).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(50).toString()))
       toRoman(n - 50)
     }
     else if (n >= 40) {
-      answer.append(romeNum.get(40).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(40).toString()))
       toRoman(n - 40)
     }
     else if (n >= 10) {
-      answer.append(romeNum.get(10).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(10).toString()))
       toRoman(n - 10)
     }
     else if (n >= 9) {
-      answer.append(romeNum.get(9).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(9).toString()))
       toRoman(n - 9)
     }
     else if (n >= 5) {
-      answer.append(romeNum.get(5).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(5).toString()))
       toRoman(n - 5)
     }
     else if (n >= 4) {
-      answer.append(romeNum.get(4).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(4).toString()))
       toRoman(n - 4)
     }
     else if (n >= 1) {
-      answer.append(romeNum.get(1).toString().replace("Some(", "").replace(")", ""))
+      answer.append(parser(romeNum.get(1).toString()))
       toRoman(n - 1)
     }
     answer.toString()
   }
 
-  // val test: Int = 3
-  // println(toRoman(test))
+  val test: Int = 255
+  println(toRoman(test))
 }
