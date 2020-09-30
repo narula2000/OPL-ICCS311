579697e281d396aef2e81da1c78fdf954279e09e
Vikrom Narula <djnarula@gmail.com>
2020-09-30 11:59:49 +0700

    Commenting var outside scope

--- Assignmet/1/src/main/scala/TurnIt.scala
+++ Assignmet/1/src/main/scala/TurnIt.scala
@@ -10,7 +10,7 @@ object TurnIt extends App {
     else
       A.map(_.head) :: transpose(A.map(_.tail))
 
-  val mtx: List[List[Int]] = List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9))
+  // val mtx: List[List[Int]] = List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9))
   // println(transpose(mtx))
   // println("Should be ->")
   // println("List(List(1, 4, 7), List(2, 5, 8), List(3, 6, 9))")
