eb024dc40ddc36e5a9f9996f415ee838f6267c8c
Vikrom Narula <djnarula@gmail.com>
2020-09-30 11:55:27 +0700

    Fixed Zombies

--- Assignmet/1/src/main/scala/Zombies.scala
+++ Assignmet/1/src/main/scala/Zombies.scala
@@ -2,36 +2,32 @@ import scala.collection.mutable
 
 object Zombies extends App {
 
-  var ans: Int = 0
-
   def countBad(hs: List[Int]): Int = {
+    var ans: Int = 0
+
+    def merge(left: List[Int], right: List[Int]): List[Int] =
+      (left, right) match {
+        case (_, Nil) => left
+        case (Nil, _) => right
+        case (leftHead :: leftTail, rightHead :: rightTail) =>
+          if (leftHead < rightHead) {
+            ans += left.length
+            rightHead :: merge(left, rightTail)
+          }
+          else
+            leftHead :: merge(leftTail, right)
+      }
+
+    def mergeSort(list: List[Int]): List[Int] = {
+      val n = list.length / 2
+      if (n == 0) list
+      else {
+        val (left, right) = list.splitAt(n)
+        merge(mergeSort(left), mergeSort(right))
+      }
+    }
     mergeSort(hs)
-    val badPair: Int = ans
-    ans = 0
-    badPair
-  }
-
-  def merge(left: List[Int], right: List[Int]): List[Int] =
-    (left, right) match {
-      case (_, Nil) => left
-      case (Nil, _) => right
-      case (leftHead :: leftTail, rightHead :: rightTail) =>
-        if (leftHead < rightHead) {
-          ans += left.length
-          rightHead :: merge(left, rightTail)
-        }
-        else {
-          leftHead :: merge(leftTail, right)
-        }
-    }
-
-  def mergeSort(list: List[Int]): List[Int] = {
-    val n = list.length / 2
-    if (n == 0) list
-    else {
-      val (left, right) = list.splitAt(n)
-      merge(mergeSort(left), mergeSort(right))
-    }
+    ans
   }
 
   // println(countBad(List(35, 22, 10)) + " == 0")
