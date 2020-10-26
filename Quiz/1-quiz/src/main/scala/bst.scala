1985ac6afbc8ba53b0f1649cd98c72547fe99ec1
Vikrom Narula <djnarula@gmail.com>
2020-10-26 23:26:17 +0700

    Wrote walkCPS in bst

--- Quiz/1-quiz/src/main/scala/bst.scala
+++ Quiz/1-quiz/src/main/scala/bst.scala
@@ -1,3 +1,29 @@
-object bst {
-  
+trait Tree
+case class Node(left: Tree, key: Int, right: Tree) extends Tree
+case object Empty extends Tree
+
+object bst extends App {
+  def walkCPS(tree: Tree): List[Int] = {
+    def walker(tree: Tree, C: List[Int] => List[Int]): List[Int] =
+      tree match {
+        case Empty => C(Nil)
+        case Node(left, key, right) =>
+          walker(
+            left,
+            leftN => {
+              walker(right, rightN => C(leftN ::: (key :: rightN)))
+            }
+          )
+      }
+    walker(tree, (f: List[Int]) => f)
+  }
+
+  // val tree = Node(
+  // Node(Empty, 2, Empty),
+  // 5,
+  // Node(Node(Empty, 6, Empty), 7, Node(Empty, 9, Empty))
+  // )
+//
+  // println(walkPreorder(tree))
 }
+
