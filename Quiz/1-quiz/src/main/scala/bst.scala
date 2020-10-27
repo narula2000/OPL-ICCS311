372e2184b8e31480c4e003b19fa3c4f8053343d2
Vikrom Narula <djnarula@gmail.com>
2020-10-27 12:33:29 +0700

    Done with test cases

--- Quiz/1-quiz/src/main/scala/bst.scala
+++ Quiz/1-quiz/src/main/scala/bst.scala
@@ -1,4 +1,4 @@
-class BinaryTree[T <: AnyVal] (val rootNode: Node) extends Tree {
+class BinaryTree[T <: AnyVal](val rootNode: Node) extends Tree {
   def walkCPS(): List[Int] = {
     def walker(tree: Tree, C: List[Int] => List[Int]): List[Int] =
       tree match {
@@ -20,13 +20,39 @@ case class Node(left: Tree, key: Int, right: Tree) extends Tree
 case object Empty extends Tree
 
 object Main extends App {
-  val tree = new BinaryTree[Int](
+  /* val tree = new BinaryTree[Int](
     Node(
       Node(Empty, 2, Empty),
       5,
-      Node(Node(Empty, 6, Empty), 7, Node(Empty, 9, Empty))
+      Node(Node(Empty, 6, Empty), 7, Node(Empty, 8, Empty))
     )
   )
+  println(tree.walkCPS()) */
 
-  println(tree.walkCPS())
+  /* val tree = new BinaryTree[Int](
+    Node(
+      Node(Node(Empty, 2, Empty), 3, Node(Empty, 4, Empty)),
+      5,
+      Node(Node(Empty, 6, Empty), 7, Node(Empty, 8, Empty))
+    )
+  )
+  println(tree.walkCPS()) */
+
+  /* val tree = new BinaryTree[Int](
+    Node(
+      Node(Node(Empty, 2, Empty), 3, Node(Empty, 4, Empty)),
+      5,
+      Node(Empty, 6, Empty)
+    )
+  )
+  println(tree.walkCPS()) */
+
+  /* val tree = new BinaryTree[Int](
+    Node(
+      Node(Node(Empty, 2, Empty), 3, Empty),
+      4,
+      Node(Empty, 5, Node(Empty, 6, Empty))
+    )
+  )
+  println(tree.walkCPS()) */
 }
