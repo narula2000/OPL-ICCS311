ce525ee7c1766380e2d552bab49b577de04f14d0
Vikrom Narula <djnarula@gmail.com>
2020-11-18 12:50:59 +0700

    finished rust and inclass12

---
+++ Inclass/12-rust/src/bin/inclass12-starter2.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let (mut str1, str2) = two_words();
+    str1 = join_words(str1, &str2);
+    println!("concatenated string is {:?}", str1);
+}
+
+fn two_words() -> (String, String) {
+    (format!("fellow"), format!("Rustaceans"))
+}
+
+/// Concatenate `suffix` onto the end of `prefix`.
+fn join_words(mut prefix: String, suffix: &String) -> String {
+    prefix.push(' '); // separate the words with a space
+    for ch in suffix.chars() {
+        prefix.push(ch);
+    }
+    prefix
+}
+
+// Goal: Convert `join_words` to use borrowing, not ownership.
+// The new function should mutate `prefix` in place,
+// and should not take ownership of `suffix`.
